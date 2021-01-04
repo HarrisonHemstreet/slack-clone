82bd6eb7915b553f2b8634ddd5956fa666b4df3c
Harrison <harrisonhemstreet@gmail.com>
2021-01-04 14:08:02 -0700

    added firebase

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,7 +11,7 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import AppsIcon from '@material-ui/icons/Apps';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMoreIcon';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
