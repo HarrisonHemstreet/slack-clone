82e538620bc50b39493c9c0862e079960dc39ded
Harrison <harrisonhemstreet@gmail.com>
2021-01-03 23:14:40 -0700

    added Header content and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's build a slack-clone</h1>
+      {/* Header */}
+      <Header />
+      {/* Sidebar */}
+      {/* React router -> chat screen */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import {Avatar} from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='header__left'>
+                <Avatar className='header__avatar' alt='{user?.displayName}' src='{user?.photoURL}'/>
+                {/* Avatar for logged in user */}
+
+                <AccessTimeIcon />
+                {/* Time Icon */}
+            </div>
+            <div className='header__search'>
+                <SearchIcon />
+                <input placeholder='Search' />
+                {/* Search Icon */}
+
+                {/* input */}
+            </div>
+            <div className='header__right'>
+                <HelpOutlineIcon />
+                {/* help icon */}
+            </div>
+        </div>
+    )
+}
+
+export default Header
