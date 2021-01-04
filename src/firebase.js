e4dffd18463e7ccedbb59b1a34ac3937731eff25
Harrison <harrisonhemstreet@gmail.com>
2021-01-04 14:53:42 -0700

    added ability to connect to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
     measurementId: "G-3D2EPMFLMY"
   };
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);\
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider
