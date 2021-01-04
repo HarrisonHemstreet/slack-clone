eb46b5db4901fa42555961376068a16a8fc77648
Harrison <harrisonhemstreet@gmail.com>
2021-01-04 13:29:13 -0700

    added more "SidebarOption" components

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -5,6 +5,7 @@ function SidebarOption({Icon, title}) {
     return (
         <div className='sidebarOption'>
             {Icon && <Icon className='sidebarOption__icon'/>}
+            {Icon ? (<h3>{title}</h3>) : (<h3><span className='sidebarOption__hash'>#</span>{title}</h3>)}
         </div>
     )
 }
