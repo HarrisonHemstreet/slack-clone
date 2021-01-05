6263064a52f35ece152909c3bdd5c4291ca23a2c
Harrison <harrisonhemstreet@gmail.com>
2021-01-04 17:52:38 -0700

    beginning to add route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build a slack-clone</h1>
-      {/* Header */}
-      <Header />
+      <Router>
+        {/* Header */}
+        <Header />
 
-      <div className='app__body'>
-        <Sidebar />
-          {/* Sidebar */}
-          {/* React router -> chat screen */}
-      </div>
-      
+        <div className='app__body'>
+          <Sidebar />
+          <Switch>
+            <Route path='/room/:roomId'>
+              <Chat />
+            </Route>
+          </Switch>
+            {/* Sidebar */}
+            {/* React router -> chat screen */}
+        </div>
+      </Router>
     </div>
   );
 }
